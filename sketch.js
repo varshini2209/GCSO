bac74e4e8d88231fdb876de618eaf528a73432e2
varshini2209 <74657573+varshini2209@users.noreply.github.com>
2020-11-30 21:52:47 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -9,30 +9,61 @@ function setup() {
   createCanvas(800,400);
 
   //creating sprites
+  //creating car sprite
   car=createSprite(50,200,50,50);
-  weight=createSprite(1500,200,60,height/2);
+  car.shapeColor="pink";
+   
+   //creating wall sprite
+   wall=createSprite(1500,200,60,height/2);
+   wall.shapeColor="white";
 
   //setting up random values
   speed=random(55,90)
   weight=random(400,1500)
 
-  //Assign the velocityX property of the car sprite to the speed variable
-  car.velocityX=speed;
+ 
 }
 
 function draw() {
-  background(255,255,255);  
+  background("black");  
   //to assign a color to the car when it collides.
-    if(wall.x-car.x<(car.width+wall.width)/2){
-      car.velocityX=0;
-      var deformation=0.5*weight*speed*speed/22509;
-      if(deformation>180){
-        car.shapeColor=color(250,0,0);
-      }
-      if(deformation<180 && deformation>100){
-        car.shapeColor
-      }
-    }
+  car.velocityX=speed;
+
+  background("black");
+
+  if(wall.x-car.x<(wall.width+car.width)/2){
+
+    car.velocityX=0;
+
+    var deformation=0.5*speed*weight*speed/22500
+
+    if(deformation>180){
+
+      car.shapeColor=color(255,0,0);
+
+  }
+  if(180>deformation&&deformation>100){
+
+    car.shapeColor=color(230,230,0);
+
+  }
+  if(deformation<100){
+
+    car.shapeColor=color(0,255,0);
+
+
+  if(car.x-wall.x<wall.width/2+car.width/2
+    && wall.x-car.x<wall.width/2+car.width/2
+    && car.y-wall.y<wall.height/2+car.height/2
+    && wall.y-car.y<wall.height/2+car.height/2){
+   
+  }
+  else{
+    car.shapeColor="pink";
+    wall.shapeColor="white";
+  }
+  }
+}
 
 
 
